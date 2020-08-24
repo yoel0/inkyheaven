7181fc779df3ff3a80c48f01d2c183ef5d058f7e
yoel <yoel@macbook-pro.home>
2020-08-24 02:51:53 -0700

    [inkyheaven] remove multer and img upload form does not work with heroku

--- server.js
+++ server.js
@@ -8,64 +8,6 @@ const SECRET_SESSION = process.env.SECRET_SESSION;
 const passport = require("./config/ppConfig");
 const flash = require("connect-flash");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const multer = require("multer");
-
-// Set The Storage Engine
-const storage = multer.diskStorage({
-  destination: "./public/uploads/",
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-
-// Init Upload
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1000000 },
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
-}).single("myImage");
-
-// Check File Type
-function checkFileType(file, cb) {
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: Images Only!");
-  }
-}
-
-app.post("/upload", (req, res) => {
-  upload(req, res, (err) => {
-    if (err) {
-      res.render("index", {
-        msg: err,
-      });
-    } else {
-      if (req.file == undefined) {
-        res.render("index", {
-          msg: "Error: No File Selected!",
-        });
-      } else {
-        res.render("index", {
-          msg: "File Uploaded!",
-          file: `uploads/${req.file.filename}`,
-        });
-      }
-    }
-  });
-});
 
 // require the authorization middleware at the top of the page
 const isLoggedIn = require("./middleware/isLoggedIn");
